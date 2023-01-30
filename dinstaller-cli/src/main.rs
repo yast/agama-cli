01a9821a067a85b6cf9b7dec4973172960edd73a
Imobach González Sosa <igonzalezsosa@suse.com>
2023-01-30 07:54:18 +0000

    feat: CLI parsing PoC
    * Playing around with Clap.
    * Partially parse the "info" and "config" subcommands.

--- dinstaller-cli/src/main.rs
+++ dinstaller-cli/src/main.rs
@@ -1,3 +1,34 @@
+use clap::Parser;
+mod commands;
+use commands::{Commands, ConfigCommands};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    pub command: commands::Commands,
+}
+
+fn info(keys: Vec<String>) {
+    unimplemented!("Display information for {:?}", &keys);
+}
+
+fn show_config(keys: Vec<String>) {
+    unimplemented!("Show config for {:?}", &keys);
+}
+
+fn set_config(values: Vec<String>) {
+    unimplemented!("Set config values {:?}", &values);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    dbg!(&cli.command);
+    match cli.command {
+        Commands::Info { keys } => info(keys),
+        Commands::Config(subcommand) => match subcommand {
+            ConfigCommands::Show { keys } => show_config(keys),
+            ConfigCommands::Set { values } => set_config(values),
+        },
+    }
 }
