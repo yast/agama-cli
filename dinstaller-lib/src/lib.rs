3c8539bcce3a9d9f11e4efe8cd2575ac7e8f84b5
Josef Reidinger <jreidinger@suse.cz>
2023-01-31 14:59:25 +0100

    use generated code from zbus-xmlgen

--- dinstaller-lib/src/lib.rs
+++ dinstaller-lib/src/lib.rs
@@ -1,28 +1,14 @@
-use zbus::blocking::{ConnectionBuilder, Proxy};
+use std::path::Path;
 
-pub fn products() -> Result<Vec<(String,String)>, zbus::Error> {
-    let connection = ConnectionBuilder::address("unix:path=/run/dbus/system_bus_socket")?.build()?;
-    let proxy = Proxy::new(&connection,
-         "org.opensuse.DInstaller.Software",
-          "/org/opensuse/DInstaller/Software1",
-        "org.opensuse.DInstaller.Software1")?;
-    proxy.get_property("AvailableBaseProducts")
-}
+// TODO: maybe expose only clients when we have it?
+pub mod proxies;
 
-mod dinstaller;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = products().expect("dbus failed");
-        assert_eq!(result, 
-            vec![
-                (String::from("ALP"), String::from("SUSE ALP ContainerHost OS")),
-                (String::from("Tumbleweed"), String::from("openSUSE Tumbleweed")),
-                (String::from("Leap Micro"), String::from("openSUSE Leap Micro 5.3")),
-                (String::from("Leap"), String::from("openSUSE Leap 15.4"))]);
-    }
+pub fn connection() -> Result<zbus::blocking::Connection, zbus::Error>{
+    let path = if Path::new("/run/d-installer/bus").exists() {
+        "/run/d-installer/bus"
+    } else {
+        "/run/dbus/system_bus_socket"
+    };
+    let address = format!("unix:path={}", path);
+    zbus::blocking::ConnectionBuilder::address(address.as_str())?.build()
 }
