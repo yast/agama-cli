b24e79df8177dea5dd3491b36b6072c5e50a95dd
Imobach González Sosa <igonzalezsosa@suse.com>
2023-03-07 07:05:05 +0000

    refactor: split the Settings macro into smaller functions

--- dinstaller-derive/src/lib.rs
+++ dinstaller-derive/src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Fields};
+use syn::{parse_macro_input, DeriveInput, Fields, Ident};
 
 #[proc_macro_derive(Settings, attributes(collection_setting))]
 pub fn dinstaller_attributes_derive(input: TokenStream) -> TokenStream {
@@ -13,50 +14,23 @@ pub fn dinstaller_attributes_derive(input: TokenStream) -> TokenStream {
         _ => panic!("only structs are supported"),
     };
 
-    let (collection, scalar): (Vec<_>, Vec<_>) = fields.iter().partition(|f| {
+    let (collection, scalar): (Vec<_>, Vec<_>) = fields.clone().into_iter().partition(|f| {
         f.attrs
             .iter()
             .any(|a| a.path.is_ident("collection_setting"))
     });
 
-    let set_field_name = scalar.iter().map(|field| &field.ident);
-    let merge_field_name = set_field_name.clone();
+    let scalar_field_names: Vec<Ident> =
+        scalar.into_iter().filter_map(|field| field.ident).collect();
 
-    let set_fn = quote! {
-        fn set(&mut self, attr: &str, value: SettingValue) -> Result<(), &'static str> {
-            match attr {
-                #(stringify!(#set_field_name) => self.#set_field_name = value.try_into()?,)*
-                _ => return Err("unknown attribute")
-            };
-            Ok(())
-        }
-    };
+    let set_fn = expand_set_fn(&scalar_field_names);
+    let merge_fn = expand_merge_fn(&scalar_field_names);
 
-    let merge_fn = quote! {
-        fn merge(&mut self, other: Self)
-        where
-            Self: Sized,
-        {
-            #(if let Some(value) = other.#merge_field_name {
-                self.#merge_field_name = Some(value.clone())
-              })*
-        }
-    };
-
-    let mut add_fn = quote! {};
-
-    if !collection.is_empty() {
-        let field_name = collection.iter().map(|field| &field.ident);
-        add_fn = quote! {
-            fn add(&mut self, attr: &str, value: SettingObject) -> Result<(), &'static str> {
-                match attr {
-                    #(stringify!(#field_name) => self.#field_name.push(value.try_into()?),)*
-                    _ => return Err("unknown attribute")
-                };
-                Ok(())
-            }
-        };
-    }
+    let collection_field_names: Vec<Ident> = collection
+        .into_iter()
+        .filter_map(|field| field.ident)
+        .collect();
+    let add_fn = expand_add_fn(&collection_field_names);
 
     let name = input.ident;
     let expanded = quote! {
@@ -67,5 +41,54 @@ pub fn dinstaller_attributes_derive(input: TokenStream) -> TokenStream {
         }
     };
 
-    TokenStream::from(expanded)
+    expanded.into()
+}
+
+fn expand_set_fn(field_name: &Vec<Ident>) -> TokenStream2 {
+    if field_name.is_empty() {
+        return quote! {};
+    }
+
+    quote! {
+        fn set(&mut self, attr: &str, value: SettingValue) -> Result<(), &'static str> {
+            match attr {
+                #(stringify!(#field_name) => self.#field_name = value.try_into()?,)*
+                _ => return Err("unknown attribute")
+            };
+            Ok(())
+        }
+    }
+}
+
+fn expand_merge_fn(field_name: &Vec<Ident>) -> TokenStream2 {
+    if field_name.is_empty() {
+        return quote! {};
+    }
+
+    quote! {
+        fn merge(&mut self, other: Self)
+        where
+            Self: Sized,
+        {
+            #(if let Some(value) = other.#field_name {
+                self.#field_name = Some(value.clone())
+              })*
+        }
+    }
+}
+
+fn expand_add_fn(field_name: &Vec<Ident>) -> TokenStream2 {
+    if field_name.is_empty() {
+        return quote! {};
+    }
+
+    quote! {
+        fn add(&mut self, attr: &str, value: SettingObject) -> Result<(), &'static str> {
+            match attr {
+                #(stringify!(#field_name) => self.#field_name.push(value.try_into()?),)*
+                _ => return Err("unknown attribute")
+            };
+            Ok(())
+        }
+    }
 }
