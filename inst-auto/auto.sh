972e0362aaf012dd5315c8f3bc36724df884decd
Josef Reidinger <jreidinger@suse.cz>
2023-03-13 22:51:50 +0100

    improvements

--- inst-auto/auto.sh
+++ inst-auto/auto.sh
@@ -8,14 +8,35 @@ else
   url="$1"
 fi
 
+if [ -z "$url" ]
+then
+  echo "no autoinstallation profile"
+  exit 0
+fi
+
 echo "Using the profile at $url"
 
 tmpdir=$(mktemp --directory --suffix "-dinstaller")
 echo working on $tmpdir
-/usr/bin/dinstaller-cli profile download $url > ${tmpdir}/profile.jsonnet
-/usr/bin/dinstaller-cli profile evaluate ${tmpdir}/profile.jsonnet > ${tmpdir}/profile.json
-/usr/bin/dinstaller-cli profile validate ${tmpdir}/profile.json
-/usr/bin/dinstaller-cli config load ${tmpdir}/profile.json
+
+case "$url" in
+*.jsonnet )
+    /usr/bin/dinstaller-cli profile download $url > ${tmpdir}/profile.jsonnet
+    /usr/bin/dinstaller-cli profile evaluate ${tmpdir}/profile.jsonnet > ${tmpdir}/profile.json
+    /usr/bin/dinstaller-cli profile validate ${tmpdir}/profile.json
+    /usr/bin/dinstaller-cli config load ${tmpdir}/profile.json
+    /usr/bin/dinstaller-cli install
+*.json )
+    /usr/bin/dinstaller-cli profile download $url > ${tmpdir}/profile.json
+    /usr/bin/dinstaller-cli profile validate ${tmpdir}/profile.json
+    /usr/bin/dinstaller-cli config load ${tmpdir}/profile.json
+    /usr/bin/dinstaller-cli install
+*.sh )
+    /usr/bin/dinstaller-cli profile download $url > ${tmpdir}/profile.sh
+    exec $env(SHELL) -c /{tmpdir}/profile.sh
+*)
+    echo "Unrecognized suffix ${url}"
+    exit 1
+esac
+
 rm -r $tmpdir
-# not implemented yet:
-# /usr/bin/dinstaller-cli watch
