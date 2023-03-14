a4a446401fc32ee693219ac2976345877c24a2eb
Josef Reidinger <jreidinger@suse.cz>
2023-03-14 17:23:31 +0100

    make shellcheck happy

--- inst-auto/auto.sh
+++ inst-auto/auto.sh
@@ -3,7 +3,7 @@ set -ex
 
 if [ -z "$1" ]
 then
-  url=`cat /proc/cmdline | awk -F 'dinst.auto=' '{sub(/ .*$/, "", $2); print $2}'`
+  url=$(awk -F 'dinst.auto=' '{sub(/ .*$/, "", $2); print $2}' < /proc/cmdline)
 else
   url="$1"
 fi
@@ -17,26 +17,26 @@ fi
 echo "Using the profile at $url"
 
 tmpdir=$(mktemp --directory --suffix "-dinstaller")
-echo working on $tmpdir
+echo "working on $tmpdir"
 
 case "$url" in
 *.jsonnet )
-    /usr/bin/dinstaller-cli profile download $url > ${tmpdir}/profile.jsonnet
-    /usr/bin/dinstaller-cli profile evaluate ${tmpdir}/profile.jsonnet > ${tmpdir}/profile.json
-    /usr/bin/dinstaller-cli profile validate ${tmpdir}/profile.json
-    /usr/bin/dinstaller-cli config load ${tmpdir}/profile.json
-    /usr/bin/dinstaller-cli install
+    /usr/bin/dinstaller-cli profile download "$url" > "${tmpdir}/profile.jsonnet"
+    /usr/bin/dinstaller-cli profile evaluate "${tmpdir}/profile.jsonnet" > "${tmpdir}/profile.json"
+    /usr/bin/dinstaller-cli profile validate "${tmpdir}/profile.json"
+    /usr/bin/dinstaller-cli config load "${tmpdir}/profile.json"
+    /usr/bin/dinstaller-cli install;;
 *.json )
-    /usr/bin/dinstaller-cli profile download $url > ${tmpdir}/profile.json
-    /usr/bin/dinstaller-cli profile validate ${tmpdir}/profile.json
-    /usr/bin/dinstaller-cli config load ${tmpdir}/profile.json
-    /usr/bin/dinstaller-cli install
+    /usr/bin/dinstaller-cli profile download "$url" > "${tmpdir}/profile.json"
+    /usr/bin/dinstaller-cli profile validate "${tmpdir}/profile.json"
+    /usr/bin/dinstaller-cli config load "${tmpdir}/profile.json"
+    /usr/bin/dinstaller-cli install;;
 *.sh )
-    /usr/bin/dinstaller-cli profile download $url > ${tmpdir}/profile.sh
-    exec $env(SHELL) -c /{tmpdir}/profile.sh
+    /usr/bin/dinstaller-cli profile download "$url" > "${tmpdir}/profile.sh"
+    exec $SHELL -c "/${tmpdir}/profile.sh";;
 *)
     echo "Unrecognized suffix ${url}"
     exit 1
 esac
 
-rm -r $tmpdir
+rm -r "$tmpdir"
